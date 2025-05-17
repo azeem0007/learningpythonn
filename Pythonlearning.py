72cc3c69436a883f0ef1f64e151cbb3e65505b6e
azeem0007 <azeemasters0007@gmail.com>
2025-05-17 14:47:01 -0400

    created band name generator project

--- Pythonlearning.py
+++ Pythonlearning.py
@@ -1,1 +1,16 @@
 
+# print("hello" + input("what is your name")  + "!")
+
+
+# print(len(input("what is your name")))
+
+
+
+# username=input("name:")
+# lenght=len(username)
+# print(lenght)
+
+print("Welcome to band name generator")
+city=input("what is the name of the city you grew up in?")
+pet=input("what is your pet name?")
+print("your band name is " + city + " " + pet)
